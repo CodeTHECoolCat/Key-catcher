64a55c8bb1e8f540d983f92deb54524d79a8012f
CodeTHECoolCat <88080899+CodeTHECoolCat@users.noreply.github.com>
2021-07-28 14:26:12 +0530

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case ")nnYAL/g|5dv6o6*!dI/":
+            case "You win":return img`
+. . . . . f f 9 9 f f . . . . . 
+. . . . f 3 3 9 9 9 9 f . . . . 
+. . . f 3 3 9 9 a a a 9 f . . . 
+. . f 3 3 9 a f f f f f 9 f . . 
+. . f 3 3 9 a f f f f f 9 f . . 
+. f 3 3 9 a f f f f f f 9 9 f . 
+. f 3 3 9 a f 7 f f 7 f 9 9 f . 
+. f 3 3 9 a f 7 f f 7 f 9 9 f . 
+. f 3 3 9 a f 5 f 5 f f 9 9 f . 
+f f 3 3 9 a f 5 f 5 f f f 9 f f 
+f 3 3 3 9 a f 5 5 5 f f f 9 a f 
+. f 3 3 9 a f f 5 f f f f a f . 
+. . d 3 9 9 9 9 5 9 9 9 9 a . . 
+. . d 5 5 5 5 5 5 5 5 5 5 d . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . . . . f f . . f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
